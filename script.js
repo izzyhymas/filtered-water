abdedae7ad9a56a722604cc2f45f9cfe396cc37c
Izzy Hymas <izzyhymas@gmail.com>
2024-06-05 11:07:53 -0600

    Fix function and styling

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const testWaterFilter = () => {
     const waterStuff = ["salt", "dirt", "calcite", "uranium", "rubber"];
+    const filterOut = ["uranium", "dirt"];
 
     console.log(`Pre filter: ${waterStuff}`);
     
-    const filteredWater = filterWater(waterStuff);
+    const filteredWater = filterWater(waterStuff, filterOut);
 
     console.log(`Post filter: ${filteredWater}`);
 };
 
 
-const filterWater = (stuffInWater) => {
+const filterWater = (stuffInWater, stuffToFilterOut) => {
     const water = [];
-    const stuffToFilterOut = ["uranium", "dirt"];
 
     for (let i = 0; i < stuffInWater.length; i++) {
         if (!stuffToFilterOut.includes(stuffInWater[i])) {
